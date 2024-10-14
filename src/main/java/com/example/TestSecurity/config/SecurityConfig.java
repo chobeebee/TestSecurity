c64c27dc79430e5fa1bc2ad94256cefd00a34f47
chobeebee <websera12@gmail.com>
2024-10-14 15:36:24 +0900

    BCrypt 암호화 메소드

--- src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity //SpringSecurity에서 관리되는 클래스로 설정
 public class SecurityConfig {
+    
+    //BCrypt 엔코드를 리턴하는 메소드
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
